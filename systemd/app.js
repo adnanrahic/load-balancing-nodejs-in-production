5de15a1db9c8873818467c1ae337b7e8d4040fbd
adnanrahic <ado.raha198@gmail.com>
2019-09-13 01:52:41 +0200

    added systemd config

---
+++ systemd/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!')
+})
+app.listen(port, () => {
+  console.log('Server running on port ', port)
+})
