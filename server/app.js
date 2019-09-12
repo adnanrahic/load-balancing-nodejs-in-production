398bcc615701629d4b6bde436e2bf76d177430be
adnanrahic <ado.raha198@gmail.com>
2019-09-13 01:52:53 +0200

    changed server config

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!')
 })
-module.exports = app
+app.listen(port, () => {
+  console.log('Server running on port ', port)
+})
--- server/server.js
+++
@@ -1,5 +0,0 @@
-const app = require('./app')
-const port = 3000
-app.listen(port, () => {
-  console.log('Server running on port ', port)
-})
