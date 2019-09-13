f6aa7c05b5af08d0f776bf91f767472e071884e8
adnanrahic <ado.raha198@gmail.com>
2019-09-13 21:05:44 +0200

    merge app and server

--- docker/app.js
+++ docker/app.js
@@ -1,7 +1,10 @@
 const os = require('os')
 const express = require('express')
 const app = express()
+const port = 3000
 app.get('/', (req, res) => {
   res.status(200).send('Hello from container: ' + os.hostname())
 })
-module.exports = app
+app.listen(port, () => {
+  console.log('Server running on ' + 'hostname: ' + os.hostname() + 'and port: ' + port)
+})
--- docker/server.js
+++
@@ -1,6 +0,0 @@
-const os = require('os')
-const app = require('./app')
-const port = 3000
-app.listen(port, () => {
-  console.log('Server running on ' + 'hostname: ' + os.hostname() + 'and port: ' + port)
-})
